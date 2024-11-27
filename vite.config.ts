5228ff3d7a3b96fe5a7010a135386331e88f6137
pietrodev07 <pietro.dev.07@gmail.com>
2024-11-27 13:51:29 +0100

    feat(path-aliases): configure path-aliases in tsconfig and vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
+import { resolve } from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+const srcAlias = {
+  find: "@",
+  replacement: resolve(__dirname, "src"),
+};
+
 // https://vite.dev/config/
 export default defineConfig({
+  resolve: { alias: [srcAlias] },
   plugins: [react()],
 });
